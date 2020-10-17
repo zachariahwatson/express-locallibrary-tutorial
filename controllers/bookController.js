980e72a6805fe22e6ad7dba360f1ddb6b32e1508
Zach Watson <54414185+zachariahwatson@users.noreply.github.com>
2020-10-16 21:51:01 -0500

    Added files and changes required for deployment to heroku

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -26,7 +26,7 @@ exports.index = function(req, res) {
             Genre.countDocuments({},callback);
         },
     }, function(err, results) {
-        res.render('index', { title: 'Local Library Home', error: err, data: results });
+        res.render('index', { title: 'Zach\'s Local Library Home', error: err, data: results });
     });
 };
 
